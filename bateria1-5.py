c9e1a3d540dfa19f57bee661517d1fc93d98560b
edgarpedret <158268605+edgarpedret@users.noreply.github.com>
2024-02-02 10:26:43 +0100

    Create bateria1-5.py

---
+++ bateria1-5.py
@@ -0,0 +1,5 @@
+print(Bateria 1-5)
+
+temp_celsius = float(input("Introdueix la temperatura en Celsius"))
+temp_fahrenheit = (temps_celsius * 9 / 5) + 32
+print("La temperatura fahrenheit és",temp_fahrenheit)
