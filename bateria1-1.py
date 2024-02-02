3a1f01bd0648b358cda6e4d15a2b66452f043ac5
edgarpedret <158268605+edgarpedret@users.noreply.github.com>
2024-02-02 10:12:55 +0100

    Update bateria1-1.py

--- bateria1-1.py
+++ bateria1-1.py
@@ -1,8 +1,6 @@
 print("Bateria 1:Exercici 1")
 
-
-
-núm1 = input("Introdueix un número: ")
-núm2 = input("Introdueix un altre número: ")
+núm1 = int(input("Introdueix un número: ")
+núm2 = int(input("Introdueix un altre número: ")
 resultat = núm1 + núm2
 print("La suma és: ",resultat)
